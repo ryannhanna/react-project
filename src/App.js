38ab08a27df341a4d774601162ca38619310286b
Ryan  Hanna <ryan.hanna15@gmail.com>
2017-12-06 16:27:41 -0600

    updated

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name='Max' age='28' />
+        <Person name="Manu" age='29' >My Hobbies: Racing</Person>
+        <Person name='Riku' age='21' />
+        <Person name='steph' age='31' />
+      </div>
+    );
+  }
+}
+
+export default App;
